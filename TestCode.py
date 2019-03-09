0bb6dc908fecedfc1c69c0e55b3df0faba3c7db2
Sudhir <sudhirsingh20@gmail.com>
2019-03-10 00:09:27 +0530

    This is branch Commit

--- TestCode.py
+++ TestCode.py
@@ -1,4 +1,4 @@
 import timeit
+import random
+print(timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
 
-print (timeit.timeit('"-".join(str(n) for n in range(100))', number=10000))
-print ("Sudh")
